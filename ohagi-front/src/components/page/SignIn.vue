c76e3c72f503554b63dd13cb9b7bf33135d92352
haton14 <iioka.takumi.quena@gmail.com>
2022-04-08 00:46:36 +0900

    Google Sign in 表示

---
+++ ohagi-front/src/components/page/SignIn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    id="g_id_onload"
+    data-client_id="aaaa"
+    data-context="signin"
+    data-ux_mode="popup"
+    data-login_uri="aa"
+    data-auto_prompt="false"
+  ></div>
+
+  <div
+    class="g_id_signin"
+    data-type="standard"
+    data-shape="pill"
+    data-theme="outline"
+    data-text="$ {button.text}"
+    data-size="medium"
+    data-logo_alignment="left"
+  ></div>
+</template>
---
+++ ohagi-front/src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <SignIn></SignIn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SignIn from "~~/src/components/page/SignIn.vue";
+</script>
