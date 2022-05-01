f87d16fec9c8bdebbe1aa9677a9f931b9206f103
haton14 <iioka.takumi.quena@gmail.com>
2022-05-01 18:56:02 +0900

    サブテスト化

--- ohagi-api/domain/vo/current_time_test.go
+++ ohagi-api/domain/vo/current_time_test.go
@@ -9,17 +9,21 @@ import (
 )
 
 func TestCurrentTime(t *testing.T) {
-	beforeTime := time.Now()
-	currentTime := vo.NewCurrentTime(context.Background())
-	afterTime := time.Now()
-	if currentTime.Value().Before(beforeTime) || currentTime.Value().After(afterTime) {
-		t.Errorf("CurrentTime.Value() is invalid")
-	}
+	t.Run("現在時刻を順に生成する", func(t *testing.T) {
+		beforeTime := time.Now()
+		currentTime := vo.NewCurrentTime(context.Background())
+		afterTime := time.Now()
+		if currentTime.Value().Before(beforeTime) || currentTime.Value().After(afterTime) {
+			t.Errorf("CurrentTime.Value() is invalid")
+		}
+	})
 
-	now := time.Now()
-	ctx := context.WithValue(context.Background(), vo.TimeKey, now)
-	currentTime = vo.NewCurrentTime(ctx)
-	if currentTime.Value() != now {
-		t.Errorf("CurrentTime.Value() is invalid")
-	}
+	t.Run("時間を指定して生成できる", func(t *testing.T) {
+		now := time.Now()
+		ctx := context.WithValue(context.Background(), vo.TimeKey, now)
+		currentTime := vo.NewCurrentTime(ctx)
+		if currentTime.Value() != now {
+			t.Errorf("CurrentTime.Value() is invalid")
+		}
+	})
 }
