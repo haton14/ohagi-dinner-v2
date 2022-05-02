c2c5c190a22e7031941f956c5e074abb073d78ba
haton14 <iioka.takumi.quena@gmail.com>
2022-05-02 21:29:59 +0900

    値型を返す

--- ohagi-api/domain/vo/role.go
+++ ohagi-api/domain/vo/role.go
@@ -8,13 +8,17 @@ type role string
 
 const OwenrRole, WritableRole, ReadonlyRole role = "owner", "writable", "readonly"
 
-func NewRole(value role) (*Role, error) {
+func NewRole(value role) (Role, error) {
 	if value != OwenrRole && value != WritableRole && value != ReadonlyRole {
-		return nil, ErrEnum
+		return Role{}, ErrEnum
 	}
-	return &Role{value: value}, nil
+	return Role{value: value}, nil
 }
 
 func (r Role) Value() role {
 	return r.value
+}
+
+func (r Role) IsZero() bool {
+	return r == Role{}
 }
